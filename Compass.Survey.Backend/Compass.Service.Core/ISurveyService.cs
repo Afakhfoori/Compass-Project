e32561977cf6fc0f26f6da5e808cf7ca32206acc
Azadeh Fakhfoori <a.fakhfoori@gmail.com>
2021-02-06 17:20:24 +1100

    Add Service project to solution

---
+++ Compass.Survey.Backend/Compass.Service.Core/ISurveyService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Compass.Domain;
+
+
+namespace Compass.Service.Core
+{
+    public interface ISurveyService
+    {
+        public IEnumerable<Survey> GetAllItems();
+        public Survey GetItemById(string id);
+    }
+}
---
+++ Compass.Survey.Backend/Compass.Service.Core/SurveyService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Compass.Repository.Core;
+using Compass.Domain;
+using System.Linq;
+
+namespace Compass.Service.Core
+{
+    public class SurveyService: ISurveyService
+    {
+        private readonly Repository<Survey> _repo;
+        public SurveyService()
+        {
+            var context = CompassContext.Instance;
+            _repo = new SurveyRepository(context);
+        }
+
+        public IEnumerable<Survey> GetAllItems()
+        {
+            var surveyList = _repo.GetAll();
+            return surveyList;
+        }
+
+        public Survey GetItemById(string id)
+        {
+            var result = _repo.GetSingle(s => s.Id == new Guid(id));
+
+            return result;
+        }
+
+    }
+}
