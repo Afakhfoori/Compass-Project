e01b4d3e53eb51246b1c4cc3e5516a451f33c861
Azadeh Fakhfoori <a.fakhfoori@gmail.com>
2021-02-06 17:22:37 +1100

    Add Api project to the solution

---
+++ Compass.Survey.Backend/Compass.Api.Core/Controllers/SurveyorsController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Compass.Domain;
+using Compass.Repository.Core;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+using Newtonsoft.Json;
+using Compass.Service.Core;
+
+namespace Compass.Api.Core.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SurveyController : ControllerBase
+    {
+
+       private readonly ISurveyService _service;
+
+        public SurveyController(ISurveyService service)
+        {
+            _service = service;
+        }
+
+        public object ReferenceHandling { get; private set; }
+
+        [HttpGet("/api/Surveys")]
+        public ActionResult<List<Survey>> Get()
+        {
+            try
+            {
+                var surveyList = _service.GetAllItems().ToList();
+                return Ok(surveyList);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500);
+            }
+
+        }
+
+        [HttpGet("/api/survey/{id}")]
+        public ActionResult<Survey> Get(string id)
+        {
+            try
+            {
+                var result = _service.GetItemById(id);
+                if (result != null)
+                {
+
+                    var settings = new Newtonsoft.Json.JsonSerializerSettings
+                    {
+                        ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                        Formatting = Formatting.Indented
+                    };
+
+                    return Ok(JsonConvert.SerializeObject(result, settings));
+                }
+                else
+                {
+                    return new NotFoundResult();
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500);
+            }
+
+        }
+    }
+}
---
+++ Compass.Survey.Backend/Compass.Api.Core/Startup.cs
@@ -0,0 +1,79 @@
+using Compass.Repository.Core;
+using Compass.Service.Core;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Compass.Api.Core
+{
+    public class Startup
+    {
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddCors(options =>
+            {
+                options.AddPolicy(MyAllowSpecificOrigins,
+                builder =>
+                {
+                builder.WithOrigins("http://localhost:3000");
+                });
+            });
+            services.AddScoped<ISurveyService, SurveyService>();
+            services.AddDbContext<CompassContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddSwaggerGen();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("v1/swagger.json", "My API V1");
+                });
+            }
+
+            app.UseCors(MyAllowSpecificOrigins);
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+           
+        }
+    }
+}
